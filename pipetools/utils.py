1b5f56360a108f910c3307128ba54a68f919ee2e
Petr Pokorny <petr@innit.cz>
2012-10-05 01:35:26 +0200

    some more docs

--- pipetools/utils.py
+++ pipetools/utils.py
@@ -27,7 +27,12 @@ def pipe_util(func):
 
 
 def auto_string_formatter(func):
+    """
+    Decorator that handles automatic string formatting.
 
+    By converting a string argument to a function that does formatting on said
+    string.
+    """
     @wraps(func)
     def auto_string_formatter_wrapper(function, *args, **kwargs):
         if isinstance(function, basestring):
@@ -45,6 +50,9 @@ def foreach(function):
     """
     Returns a function that takes an iterable and returns an iterator over the
     results of calling `function` on each item of the iterable.
+
+    >>> xrange(5) > foreach(factorial) | list
+    [1, 1, 2, 6, 24]
     """
     return partial(imap, function)
 
@@ -54,6 +62,13 @@ def foreach_do(function):
     """
     Like :func:`foreach` but is evaluated immediately and doesn't return
     anything.
+
+    For the occasion that you just want to do some side-effects::
+
+        open('addresses.txt') > foreach(geocode) | foreach_do(launch_missile)
+
+    -- With :func:`foreach` nothing would happen (except an itetrator being
+    created)
     """
     def f(iterable):
         for item in iterable:
@@ -65,11 +80,11 @@ def foreach_do(function):
 @pipe_util
 def where(function):
     """
-    Pipe-able lazy filter::
+    Pipe-able lazy filter.
 
-        >>> odd = pipe | xrange | where(lambda x: x % 2) | tuple
-        >>> odd(10)
-        (1, 3, 5, 7, 9)
+    >>> odd_range = xrange | where(X % 2) | list
+    >>> odd_range(10)
+    [1, 3, 5, 7, 9]
 
     """
     return partial(ifilter, function)
@@ -85,8 +100,23 @@ def where_not(function):
 
 @pipe_util
 @data_structure_builder
-def sort_by(key):
-    return partial(sorted, key=key)
+def sort_by(function):
+    """
+    Sorts an incoming sequence by using the given `function` as key.
+
+    >>> xrange(10) > sort_by(-X)
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+    Supports automatic data-structure creation::
+
+        users > sort_by([X.last_name, X.first_name])
+
+    There is also a shortcut for ``sort_by(X)`` called ``sort``:
+
+    >>> [4, 5, 8, -3, 0] > sort
+    [-3, 0, 4, 5, 8]
+    """
+    return partial(sorted, key=function)
 
 sort = sort_by(X)
 
@@ -94,19 +124,46 @@ sort = sort_by(X)
 @pipe_util
 @auto_string_formatter
 @data_structure_builder
-def debug_print(func):
+def debug_print(function):
+    """
+    Prints function applied on input and returns the input.
+
+    ::
+
+        foo = (pipe
+            | something
+            | debug_print(X.get_status())
+            | something_else
+            | foreach(debug_print("attr is: {0.attr}"))
+            | etc)
+    """
     def debug(thing):
-        print func(thing)
+        print function(thing)
         return thing
     return debug
 
 
 @pipe_util
-def as_args(func):
-    return lambda x: func(*x)
+def as_args(function):
+    """
+    Applies the sequence in the input as positional arguments to `function`.
+
+    ::
+
+        some_lists > as_args(izip)
+    """
+    return lambda x: function(*x)
 
 
 def take_first(count):
+    """
+    Assumes an iterable on the input, returns an iterable with first `count`
+    items from the input (or possibly less, if there isn't that many).
+
+    >>> xrange(9000) > where(X % 100 == 0) | take_first(5) | tuple
+    (0, 100, 200, 300, 400)
+
+    """
     def _take_first(iterable):
         return islice(iterable, count)
     return pipe | _take_first
@@ -114,18 +171,27 @@ def take_first(count):
 
 @pipe_util
 def select_first(condition):
+    """
+    Returns first item from input sequence that satisfies `condition`. Or
+    ``None`` if none does.
+
+    >>> ['py', 'pie', 'pi'] > select_first(X.startswith('pi'))
+    'pie'
+    """
     return lambda seq: unless(StopIteration, where(condition) | X.next(), seq)
 
 
 @pipe_util
 @auto_string_formatter
 @data_structure_builder
-def group_by(func):
-
+def group_by(function):
+    """
+    Returns a dictionary of input sequence items grouped by `function`.
+    """
     def _group_by(seq):
         result = {}
         for item in seq:
-            result.setdefault(func(item), []).append(item)
+            result.setdefault(function(item), []).append(item)
         return result
 
     return _group_by
@@ -148,10 +214,16 @@ def _flatten(x):
 
 
 def flatten(*args):
+    """
+    Flattens an arbitrarily deep nested iterable(s).
+    """
     return _flatten(args)
 flatten = pipe | flatten
 
 
 def count(iterable):
+    """
+    Returns the number of items in `iterable`.
+    """
     return sum(1 for whatever in iterable)
 count = pipe | count
