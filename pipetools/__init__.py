448d4dc7e45ccbac762506d071d95395c4625257
Arnie97 <arnie97@gmail.com>
2016-08-02 19:23:08 +0800

    prevent namespace pollution

--- pipetools/__init__.py
+++ pipetools/__init__.py
@@ -1,8 +1,13 @@
 from pipetools.utils import foreach
 
 __version__ = VERSION = 0, 2, 7
-
 __versionstr__ = VERSION > foreach(str) | '.'.join
 
 from pipetools.main import pipe, X, maybe, xpartial
 from pipetools.utils import *
+
+# prevent namespace pollution
+import pipetools.compat
+for symbol in dir(pipetools.compat):
+    if globals().get(symbol) is getattr(pipetools.compat, symbol):
+        globals().pop(symbol)
